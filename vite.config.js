4c64f7a84e38aa767b4f0fd91e2c283d8d8aec82
imreegall <imreegall@gmail.com>
2022-10-13 02:05:59 +0300

    second commit

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/vite-vue-nft/',
   publicPath: '/vite-vue-nft/',
   i18n: {
     seo: true
